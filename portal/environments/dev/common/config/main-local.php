5af3a334281bfb150c32d447441c490b1ef33c04
Nur Aziz <naziz0404@gmail.com>
2022-06-14 22:53:29 +0700

    .

--- portal/environments/dev/common/config/main-local.php
+++ portal/environments/dev/common/config/main-local.php
@@ -1,12 +1,19 @@
 <?php
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../../");
+$dotenv->load();
+
+$db_host = $_ENV['DB_HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASS'];
+$db_name = $_ENV['DB_NAME'];
 
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host='.$db_host.';dbname='.$db_name,
+            'username' => $db_user,
+            'password' => $db_pass,
             'charset' => 'utf8',
         ],
         'mailer' => [
