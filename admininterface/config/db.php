e82fcbeee8ebf6a222013c5e4a523e8e59ef9111
Nur Aziz <naziz0404@gmail.com>
2022-06-14 10:35:27 +0700

    initial

---
+++ admininterface/config/db.php
@@ -0,0 +1,22 @@
+<?php
+require "../../vendor/autoload.php";
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../../");
+$dotenv->load();
+
+$db_host = $_ENV['DB_HOST'];
+$db_user = $_ENV['DB_USER'];
+$db_pass = $_ENV['DB_PASS'];
+$db_name = $_ENV['DB_NAME'];
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host='.$db_host.';dbname='.$db_name,
+    'username' => $db_user,
+    'password' => $db_name,
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
---
+++ admininterface/views/site/index.php
@@ -0,0 +1,37 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Zero Store Admin';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Welcome Back, Admin!</h1>
+
+        <p class="lead">Don't forget to always monitor global topup game price updates.</p>
+
+        <p><a class="btn btn-lg btn-dark" href="https://api.whatsapp.com/send?phone=628816263425">Contact Owner Via WhatsApp</a></p>
+        
+        <p><a class="btn btn-md btn-dark" href="wwww.instagram.com/zeromlbbshop">Our Instagram</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-sm-3">
+                <img class="img-fluid img-thumbnail" src="<?= Yii::$app->request->baseUrl ?>/images/diamondml.png" alt="...">
+            </div>
+            <div class="col-sm-3">
+                <img class="img-fluid img-thumbnail" src="<?= Yii::$app->request->baseUrl ?>/images/diamondfreefire.png" alt="...">
+            </div>
+            <div class="col-sm-3">
+                <img class="img-fluid img-thumbnail" src="<?= Yii::$app->request->baseUrl ?>/images/jokiml.png" alt="...">
+            </div>
+            <div class="col-sm-3">
+                <img class="img-fluid img-thumbnail" src="<?= Yii::$app->request->baseUrl ?>/images/Starlight.png" alt="...">
+            </div>
+        </div>
+
+    </div>
+</div>
